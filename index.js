754bac22a8155ab2fdc5232153622022604559ee
Juan Barrera <juanpablo.is@outlook.com>
2020-08-29 12:42:48 -0500

    Fix map coordinates.

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 var canvas = document.getElementById("myCanvas");
-var colorDots = 'green';
+var colorDots = 'rgba(10, 11, 83, .5)';
 var valueCycle = 10000;
+var scale = 70;
 
 createMap();
-createShape1();
+createRectangleBorder()
+// createShape1();
 createCircle();
+// processSimulate();
 
-for (let i = 0; i < valueCycle; i++) {
-    // createPoint();
+function processSimulate() {
+    for (let i = 0; i < valueCycle; i++) {
+        createPoint();
+    }
+}
+
+function createRectangleBorder() {
+    var ctx = canvas.getContext("2d");
+    ctx.strokeStyle = 'rgb(191, 23, 27)';
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.rect(0, 0, 700, 700);
+    ctx.stroke();
+    ctx.stroke();
 }
 
 function createMap() {
     // Set graph
-    var width = 1035, height = 1020;
+    var width = 725, height = 725;
 
     // create an svg container
     var vis = d3.select("#graph")
         .append("svg:svg")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", width + 25)
+        .attr("height", height + 25);
 
-    var xScale = d3.scale.linear().domain([10, 0]).range([width - 10, 25]);
-    var yScale = d3.scale.linear().domain([0, 5]).range([height / 2, 10]);
+    var xScale = d3.scale.linear().domain([10, 0]).range([width, 25]);
+    var yScale = d3.scale.linear().domain([0, 10]).range([width, 25]);
 
     // define the y axis
     var yAxis = d3.svg.axis()
@@ -35,7 +50,7 @@ function createMap() {
 
     var xAxisPlot = vis.append("g")
         .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + (height - 510) + ")")
+        .attr("transform", "translate(0," + (height) + ")")
         .call(xAxis.tickSize(0, 0, 0));
 
     var yAxisPlot = vis.append("g")
@@ -45,19 +60,19 @@ function createMap() {
 
 
     xAxisPlot.selectAll(".tick line")
-        .attr("y1", -500);
+        .attr("y1", -700);
 
     yAxisPlot.selectAll(".tick line")
-        .attr("x2", 1000);
+        .attr("x2", 700);
 }
 
 function createCircle() {
     var ctxDot2 = canvas.getContext("2d");
     ctxDot2.fillStyle = "transparent";
-    ctxDot2.strokeStyle = '#434745';
-    ctxDot2.lineWidth = 2;
+    ctxDot2.strokeStyle = 'rgb(191, 23, 27)';
+    ctxDot2.lineWidth = 1;
     ctxDot2.beginPath();
-    ctxDot2.arc(500, 250, 240, 0, Math.PI * 2, true);
+    ctxDot2.arc(scale * 5, scale * 5, scale * 5, 0, Math.PI * 2, true);
     ctxDot2.fill();
     ctxDot2.stroke();
 
@@ -66,7 +81,7 @@ function createCircle() {
     ctxDotCenter.strokeStyle = '#434745';
     ctxDotCenter.lineWidth = 2;
     ctxDotCenter.beginPath();
-    ctxDotCenter.arc(500, 250, 4, 0, Math.PI * 2, true);
+    ctxDotCenter.arc(scale * 5, scale * 5, 4, 0, Math.PI * 2, true);
     ctxDotCenter.fill();
     ctxDotCenter.stroke();
 }
@@ -78,40 +93,40 @@ function createShape1() {
     ctx.lineWidth = 2;
 
     ctx.moveTo(0, 0);
-    ctx.lineTo(200, 500);
+    ctx.lineTo(scale * 2, scale * 5);
     ctx.stroke();
 
     // Create second line
     var ctx2 = canvas.getContext("2d");
-    ctx2.moveTo(200, 500);
-    ctx2.lineTo(800, 400);
+    ctx2.moveTo(scale * 2, scale * 5);
+    ctx2.lineTo(scale * 8, scale * 4);
     ctx2.stroke();
 
     // Create third line
     var ctx3 = canvas.getContext("2d");
-    ctx3.moveTo(800, 400);
-    ctx3.lineTo(1000, 0);
+    ctx3.moveTo(scale * 8, scale * 4);
+    ctx3.lineTo(scale * 10, 0);
     ctx3.stroke();
 
-    // Create first dot
-    var ctxDot2 = canvas.getContext("2d");
-    ctxDot2.fillStyle = "#0B6AB1";
-    ctxDot2.strokeStyle = '#434745';
-    ctxDot2.lineWidth = 2;
-    ctxDot2.beginPath();
-    ctxDot2.arc(800, 400, 5, 0, Math.PI * 2, true);
-    ctxDot2.fill();
-    ctxDot2.stroke();
+    // // Create first dot
+    // var ctxDot2 = canvas.getContext("2d");
+    // ctxDot2.fillStyle = "#0B6AB1";
+    // ctxDot2.strokeStyle = '#434745';
+    // ctxDot2.lineWidth = 2;
+    // ctxDot2.beginPath();
+    // ctxDot2.arc(scale * 8, scale * 4, 5, 0, Math.PI * 2, true);
+    // ctxDot2.fill();
+    // ctxDot2.stroke();
 
-    // Create second dot
-    var ctxDot1 = canvas.getContext("2d");
-    ctxDot1.fillStyle = "#0B6AB1";
-    ctxDot1.strokeStyle = '#434745';
-    ctxDot1.lineWidth = 2;
-    ctxDot1.beginPath();
-    ctxDot1.arc(200, 500, 5, 0, Math.PI * 2, true);
-    ctxDot1.fill();
-    ctxDot1.stroke();
+    // // Create second dot
+    // var ctxDot1 = canvas.getContext("2d");
+    // ctxDot1.fillStyle = "#0B6AB1";
+    // ctxDot1.strokeStyle = '#434745';
+    // ctxDot1.lineWidth = 2;
+    // ctxDot1.beginPath();
+    // ctxDot1.arc(scale * 2, scale * 5, 5, 0, Math.PI * 2, true);
+    // ctxDot1.fill();
+    // ctxDot1.stroke();
 }
 
 function createPoint() {
