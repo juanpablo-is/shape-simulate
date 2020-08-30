4c421f4c83fb1a912e4479cfa65a5594c26d4cd1
Juan Barrera <juanpablo.is@outlook.com>
2020-08-30 17:25:39 -0500

    Add algorithm to calculate point in polygon.

---
+++ create.js
@@ -0,0 +1,111 @@
+
+
+function createPoint(point) {
+    var ctxDot = canvasRectangle.getContext("2d");
+    ctxDot.fillStyle = colorDots;
+    ctxDot.beginPath();
+    ctxDot.arc(point.x, point.y, 2, 0, Math.PI * 2, true);
+    ctxDot.fill();
+}
+
+function createCircle() {
+    var canvasCircle = canvas.getContext("2d");
+    canvasCircle.fillStyle = "transparent";
+    canvasCircle.strokeStyle = 'rgb(191, 23, 27)';
+    canvasCircle.lineWidth = 1;
+    canvasCircle.beginPath();
+    canvasCircle.arc(scale * 5, scale * 5, scale * 5, 0, Math.PI * 2, true);
+    canvasCircle.fill();
+    canvasCircle.stroke();
+
+    var ctxDotCenter = canvas.getContext("2d");
+    ctxDotCenter.fillStyle = "#434745";
+    ctxDotCenter.strokeStyle = '#434745';
+    ctxDotCenter.lineWidth = 2;
+    ctxDotCenter.beginPath();
+    ctxDotCenter.arc(scale * 5, scale * 5, 4, 0, Math.PI * 2, true);
+    ctxDotCenter.fill();
+    ctxDotCenter.stroke();
+}
+
+function createShape1() {
+    // Create first line
+    var ctx = canvas.getContext("2d");
+    ctx.strokeStyle = '#434745';
+    ctx.lineWidth = 2;
+
+    for (let i = 0; i < verticesShape1.length; i++) {
+        //Create a line.
+        var ctx2 = canvas.getContext("2d");
+        ctx2.moveTo(scale * verticesShape1[i]['x'], scale * verticesShape1[i]['y']);
+        ctx2.lineTo(scale * verticesShape1[i == verticesShape1.length - 1 ? 0 : i + 1]['x'], scale * verticesShape1[i == verticesShape1.length - 1 ? 0 : i + 1]['y']);
+        ctx2.stroke();
+
+        //Create a dot.
+        var ctxDot2 = canvas.getContext("2d");
+        ctxDot2.fillStyle = "#0B6AB1";
+        ctxDot2.strokeStyle = '#434745';
+        ctxDot2.lineWidth = 2;
+        // Dot start.
+        ctxDot2.beginPath();
+        ctxDot2.arc(scale * verticesShape1[i == verticesShape1.length - 1 ? 0 : i + 1]['x'], scale * verticesShape1[i == verticesShape1.length - 1 ? 0 : i + 1]['y'], 5, 0, Math.PI * 2, true);
+        ctxDot2.fill();
+        ctxDot2.stroke();
+        //Dot end.
+        ctxDot2.beginPath();
+        ctxDot2.arc(scale * verticesShape1[i == verticesShape1.length - 1 ? 0 : i + 1]['x'], scale * verticesShape1[i == verticesShape1.length - 1 ? 0 : i + 1]['y'], 5, 0, Math.PI * 2, true);
+        ctxDot2.fill();
+        ctxDot2.stroke();
+    }
+}
+
+function createMap() {
+    // Set graph
+    var width = 725, height = 725;
+
+    // create an svg container
+    var vis = d3.select("#graph")
+        .append("svg:svg")
+        .attr("width", width + 25)
+        .attr("height", height + 25);
+
+    var xScale = d3.scale.linear().domain([10, 0]).range([width, 25]);
+    var yScale = d3.scale.linear().domain([0, 10]).range([width, 25]);
+
+    // define the y axis
+    var yAxis = d3.svg.axis()
+        .orient("left")
+        .scale(yScale);
+
+    // define the y axis
+    var xAxis = d3.svg.axis()
+        .orient("bottom")
+        .scale(xScale);
+
+    var xAxisPlot = vis.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + (height) + ")")
+        .call(xAxis.tickSize(0, 0, 0));
+
+    var yAxisPlot = vis.append("g")
+        .attr("class", "axis axis--y")
+        .attr("transform", "translate(" + (25) + ",0)")
+        .call(yAxis.tickSize(0, 0, 0));
+
+
+    xAxisPlot.selectAll(".tick line")
+        .attr("y1", -700);
+
+    yAxisPlot.selectAll(".tick line")
+        .attr("x2", 700);
+}
+
+function createRectangleBorder(width = 700, height = 700) {
+    var ctx = canvasRectangle.getContext("2d");
+    ctx.clearRect(0, 0, 700, 700);
+    ctx.strokeStyle = 'rgb(191, 23, 27)';
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.rect(0, 0, width, height);
+    ctx.stroke();
+}
--- index.js
+++ index.js
@@ -15,8 +15,15 @@ let sizeY = 10;
 
 var timing = null;
 
+let verticesShape1 = [
+    { 'x': 0, 'y': 0 },
+    { 'x': 2, 'y': 5 },
+    { 'x': 8, 'y': 4 },
+    { 'x': 10, 'y': 0 },
+];
+
 createMap();
-createRectangleBorder()
+createRectangleBorder();
 
 selectShape.onchange = function () {
     clearTimeout(timing);
@@ -64,7 +71,7 @@ function processSimulate() {
     if (selectShape.value == 1)
         areaExpected = Math.PI * Math.pow(350, 2);
     else if (selectShape.value == 2)
-        areaExpected = 24500 + 14700 + 117600 + 19600;
+        areaExpected = calculateArea(); // 176400;
     document.getElementById('info-expected').innerHTML = areaExpected.toFixed(2);
     document.getElementById('info-total').innerHTML = valueCycle;
 
@@ -72,136 +79,61 @@ function processSimulate() {
     for (let i = 0; i < valueCycle; i++) {
         setTimeout(function () {
             let x = Math.random() * (sizeX * scale), y = Math.random() * (sizeY * scale);
+            let point = { 'x': x, 'y': y };
+            createPoint(point);
+
             if (selectShape.value == 1) {
                 if (Math.pow(x - 350, 2) + Math.pow(y - 350, 2) <= Math.pow(350, 2))
                     dentro++;
-                document.getElementById('info-result').innerHTML = (areaRectangle * (dentro / valueCycle)).toFixed(2);
-                document.getElementById('info-inside').innerHTML = dentro;
             }
+            else if (selectShape.value == 2) {
+                if (pointInside(point))
+                    dentro++;
+            }
+            document.getElementById('info-result').innerHTML = (areaRectangle * (dentro / valueCycle)).toFixed(2);
+            document.getElementById('info-inside').innerHTML = dentro;
 
-            createPoint(x, y);
         }, 0);
     }
 }
 
-function createCircle() {
-    var canvasCircle = canvas.getContext("2d");
-    canvasCircle.fillStyle = "transparent";
-    canvasCircle.strokeStyle = 'rgb(191, 23, 27)';
-    canvasCircle.lineWidth = 1;
-    canvasCircle.beginPath();
-    canvasCircle.arc(scale * 5, scale * 5, scale * 5, 0, Math.PI * 2, true);
-    canvasCircle.fill();
-    canvasCircle.stroke();
+function calculateArea() {
+    let vertices = JSON.parse(JSON.stringify(verticesShape1));
+    vertices.push(vertices[0]);
 
-    var ctxDotCenter = canvas.getContext("2d");
-    ctxDotCenter.fillStyle = "#434745";
-    ctxDotCenter.strokeStyle = '#434745';
-    ctxDotCenter.lineWidth = 2;
-    ctxDotCenter.beginPath();
-    ctxDotCenter.arc(scale * 5, scale * 5, 4, 0, Math.PI * 2, true);
-    ctxDotCenter.fill();
-    ctxDotCenter.stroke();
+    let count = vertices.length - 1;
+    let valueLeft = 0, valueRight = 0;
+
+    for (let i = 0; i < count; i++) {
+        valueLeft += vertices[i]['y'] * vertices[i + 1]['x'] * scale * scale;
+        valueRight += vertices[i]['x'] * vertices[i + 1]['y'] * scale * scale;
+    }
+
+    let result = Math.abs((valueLeft - valueRight) / 2);
+    return result;
 }
 
-function createShape1() {
-    // Create first line
-    var ctx = canvas.getContext("2d");
-    ctx.strokeStyle = '#434745';
-    ctx.lineWidth = 2;
+function pointInside(point) {
+    point.x = point.x / 70;
+    point.y = point.y / 70;
 
-    ctx.moveTo(0, 0);
-    ctx.lineTo(scale * 2, scale * 5);
-    ctx.stroke();
-
-    // Create second line
-    var ctx2 = canvas.getContext("2d");
-    ctx2.moveTo(scale * 2, scale * 5);
-    ctx2.lineTo(scale * 8, scale * 4);
-    ctx2.stroke();
-
-    // Create third line
-    var ctx3 = canvas.getContext("2d");
-    ctx3.moveTo(scale * 8, scale * 4);
-    ctx3.lineTo(scale * 10, 0);
-    ctx3.stroke();
-
-    // Create first dot
-    var ctxDot2 = canvas.getContext("2d");
-    ctxDot2.fillStyle = "#0B6AB1";
-    ctxDot2.strokeStyle = '#434745';
-    ctxDot2.lineWidth = 2;
-    ctxDot2.beginPath();
-    ctxDot2.arc(scale * 8, scale * 4, 5, 0, Math.PI * 2, true);
-    ctxDot2.fill();
-    ctxDot2.stroke();
-
-    // Create second dot
-    var ctxDot1 = canvas.getContext("2d");
-    ctxDot1.fillStyle = "#0B6AB1";
-    ctxDot1.strokeStyle = '#434745';
-    ctxDot1.lineWidth = 2;
-    ctxDot1.beginPath();
-    ctxDot1.arc(scale * 2, scale * 5, 5, 0, Math.PI * 2, true);
-    ctxDot1.fill();
-    ctxDot1.stroke();
-}
-
-function createMap() {
-    // Set graph
-    var width = 725, height = 725;
-
-    // create an svg container
-    var vis = d3.select("#graph")
-        .append("svg:svg")
-        .attr("width", width + 25)
-        .attr("height", height + 25);
-
-    var xScale = d3.scale.linear().domain([10, 0]).range([width, 25]);
-    var yScale = d3.scale.linear().domain([0, 10]).range([width, 25]);
-
-    // define the y axis
-    var yAxis = d3.svg.axis()
-        .orient("left")
-        .scale(yScale);
-
-    // define the y axis
-    var xAxis = d3.svg.axis()
-        .orient("bottom")
-        .scale(xScale);
-
-    var xAxisPlot = vis.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + (height) + ")")
-        .call(xAxis.tickSize(0, 0, 0));
-
-    var yAxisPlot = vis.append("g")
-        .attr("class", "axis axis--y")
-        .attr("transform", "translate(" + (25) + ",0)")
-        .call(yAxis.tickSize(0, 0, 0));
-
-
-    xAxisPlot.selectAll(".tick line")
-        .attr("y1", -700);
-
-    yAxisPlot.selectAll(".tick line")
-        .attr("x2", 700);
-}
-
-function createRectangleBorder(width = 700, height = 700) {
-    var ctx = canvasRectangle.getContext("2d");
-    ctx.clearRect(0, 0, 700, 700);
-    ctx.strokeStyle = 'rgb(191, 23, 27)';
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.rect(0, 0, width, height);
-    ctx.stroke();
-}
-
-function createPoint(x, y) {
-    var ctxDot = canvasRectangle.getContext("2d");
-    ctxDot.fillStyle = colorDots;
-    ctxDot.beginPath();
-    ctxDot.arc(x, y, 2, 0, Math.PI * 2, true);
-    ctxDot.fill();
+    let intersections = 0;
+    for (let i = 1; i < verticesShape1.length; i++) {
+        let vertex1 = verticesShape1[i - 1];
+        let vertex2 = verticesShape1[i];
+        if (vertex1['y'] == vertex2['y'] && vertex1['y'] == point['y'] && point['x'] > Math.min(vertex1['x'], vertex2['x']) && point['x'] < Math.max(vertex1['x'], vertex2['x']))
+            return true; //boundary
+        if (point['y'] > Math.min(vertex1['y'], vertex2['y']) && point['y'] <= Math.max(vertex1['y'], vertex2['y']) && point['x'] <= Math.max(vertex1['x'], vertex2['x']) && vertex1['y'] != vertex2['y']) {
+            xinters = (point['y'] - vertex1['y']) * (vertex2['x'] - vertex1['x']) / (vertex2['y'] - vertex1['y']) + vertex1['x'];
+            if (xinters == point['x'])
+                return true; //boundary
+            if (vertex1['x'] == vertex2['x'] || point['x'] <= xinters)
+                intersections++;
+        }
+    }
+    if (intersections % 2 != 0) {
+        return true; //inside
+    } else {
+        return false; //outside
+    }
 }
